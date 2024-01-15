580b7c88328e2a07c63bbdc26355754860b3d401
ZekaCedar <zulaikha.affandi@gmail.com>
2024-01-15 18:11:41 +0800

    Building microservices using Spring Boot - Complete all CRUD methods

--- section2/accounts/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ section2/accounts/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -9,4 +9,22 @@ public interface IAccountsService {
      * @param customerDto
      */
     void createAccount(CustomerDto customerDto);
+
+    /**
+     * @param mobileNumber
+     * @return Account Details based on a given mobile number
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     * @param customerDto
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     * @param mobileNumber
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
 }
