7bb496e8ec715c698bdead6c4568977e3a316e00
ZekaCedar <zulaikha.affandi@gmail.com>
2024-01-29 09:21:08 +0800

    Added new microservices for cards and loans

--- section2/accounts/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section2/accounts/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,9 +1,39 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Siti Zulaikha Binti Mohd Affandi",
+						email = "zulaikha.affandi@gmail.com",
+						url = "https://www.linkedin.com/in/zulaikha-affandi/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/zulaikha-affandi/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- section2/accounts/accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ section2/accounts/accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,75 @@
 package com.eazybytes.accounts.exception;
 
 import com.eazybytes.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Exception thrown when validation annotated @Valid or Validation related annotations fails
+     * Validation defined from the dto like @NotEmpty, @Email, @Pattern...
+     *
+     * Customize the handling of {@link MethodArgumentNotValidException}.
+     * <p>This method delegates to {@link #handleExceptionInternal}.
+     *
+     * @param ex      the exception to handle
+     * @param headers the headers to be written to the response
+     * @param status  the selected response status
+     * @param request the current request
+     * @return a {@code ResponseEntity} for the response to use, possibly
+     * {@code null} when the response is already committed
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * For Runtime error, can be enhance to trigger an email to operation or
+     * make an entry into one of the table
+     *
+     * How to Test RunTime Exception? Remove @AllArgsConstructor from AccountsController
+     * causing no Autowiring for AccountsService resulting a NullPointException
+     *
+     * @param exception
+     * @param webRequest
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false), // what is the api path client is trying to invoke
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
